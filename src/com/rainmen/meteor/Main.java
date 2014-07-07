082474a6d3413c8b5de986845581744de31ab5ab
AlmasB <toll.everon@gmail.com>
2014-07-07 19:01:19 +0500

    initial setup

---
+++ src/com/rainmen/meteor/Main.java
@@ -0,0 +1,14 @@
+package com.rainmen.meteor;
+
+/**
+ * This is the entry point to Meteor
+ *
+ * @author see AUTHORS file
+ * @version 1.0
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
