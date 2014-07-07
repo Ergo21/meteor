41818cebd5a6af716ee337ad92b25cc44f7e642e
Ergo21 <jww14@uni.brighton.ac.uk>
2014-07-07 16:53:09 +0100

    Created basic WeatherData class

--- src/com/rainmen/meteor/WeatherData.java
+++ src/com/rainmen/meteor/WeatherData.java
@@ -8,5 +8,26 @@ package com.rainmen.meteor;
  *
  */
 public class WeatherData {
+	public int longitude;
+	public int latitude;
+	public String country = "NONE";
+	public double sunrise = -1;	//-1 = not set
+	public double sunset  = -1;	//-1 = not set
+	public String description;
+	public double temperature;
+	public int humidity;
+	public int pressure;
+	public double minTemp;
+	public double maxTemp;
+	public double windSpeed = -1;	//-1 = not set
+	public double windDirection = -1; //-1 = not set
+	public double cloudPercent = -1;  //-1 = not set
+	public String name = "None";
+
+	public WeatherData(int log, int lat, String desc) {
+		longitude = log;
+		latitude  = lat;
+		description = desc;
+	}
 
 }
